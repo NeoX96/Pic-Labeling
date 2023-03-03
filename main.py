811d7defbc30820bc83168e03c5c20c804cdcf03
Valentin <valentin@wuerfelmail.de>
2023-03-03 22:48:52 +0100

    new intial push

--- main.py
+++ main.py
@@ -1,89 +1,47 @@
 import tkinter as tk
-import os
-from tkinter import ttk
-import cv2
-from PIL import Image, ImageTk
-from utils.camera import Camera, get_next_file_name, save_image
+from tkinter import messagebox
+from classes.videocapture import VideoCapture
+from classes.imageprocessing import ImageProcessing
 
+class MainApplication(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.title("Image Capture Tool")
+        self.config(bg='#2E2E2E')
+        
+        self.video_capture = VideoCapture(self)
+        self.image_processing = ImageProcessing(self)
 
-class App:
-    def __init__(self, window, camera_index=0):
-        self.window = window
-        self.window.title("KI-Pic-Labeling")
-        self.camera = Camera(camera_index)
+        self.label_image = tk.Label(self, text="Enter Image Label:", bg='#2E2E2E', fg='white')
+        self.entry_image = tk.Entry(self, bg='#333333', fg='white')
+        self.label_interval = tk.Label(self, text="Enter Capture Interval (ms):", bg='#2E2E2E', fg='white')
+        self.entry_interval = tk.Entry(self, bg='#333333', fg='white')
+        self.button = tk.Button(self, text="Start Capture", command=self.start_capture, bg='#00FF00', fg='black')
+        
+        self.label_image.pack(pady=10)
+        self.entry_image.pack(pady=10)
+        self.label_interval.pack(pady=10)
+        self.entry_interval.pack(pady=10)
+        self.button.pack(pady=10)
 
-        self.label_entry = ttk.Entry(self.window)
-        self.label_entry.grid(row=0, column=0, padx=5, pady=5)
-        self.label_entry.focus()
-
-        self.capture_button = ttk.Button(self.window, text="Capture", command=self.start_capture)
-        self.capture_button.grid(row=0, column=1, padx=5, pady=5)
-
-        self.preview_label = ttk.Label(self.window)
-        self.preview_label.grid(row=1, column=0, columnspan=2, padx=5, pady=5)
-
-        self.is_capturing = False
-        self.timer_id = None
-        self.interval = 100 # Time between captures in ms
-
-        # Display Video Feed in GUI
-        self.update()
-
-    def update(self):
-        frame = self.camera.get_frame()
-
-        if frame is not None:
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            image = Image.fromarray(frame)
-            self.photo = ImageTk.PhotoImage(image)
-            self.preview_label.configure(image=self.photo)
-
-        self.window.after(50, self.update)
-
-
+        self.video_capture.pack(pady=10)
 
     def start_capture(self):
-        label = self.label_entry.get()
-        folder_path = os.path.join("captures", label)
-
-        # Create folder if it doesn't exist
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
-
-        self.is_capturing = True
-        self.capture_button.configure(text="Stop Capture", command=self.stop_capture)
-
-        def capture():
-            frame = self.camera.get_frame()
-
-            if frame is None:
-                self.stop_capture()
-                return
-
-            file_name = get_next_file_name(label, folder_path)
-            save_image(frame, file_name)
-
-            # Display preview
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            image = Image.fromarray(frame)
-            self.photo = ImageTk.PhotoImage(image)
-            self.preview_label.configure(image=self.photo)
-
-            self.timer_id = self.window.after(self.interval, capture)
-
-        capture()
-
+        label = self.entry_image.get()
+        if label:
+            interval = self.entry_interval.get()
+            if interval:
+                self.image_processing.interval = int(interval)
+            self.image_processing.label = label
+            self.image_processing.start_capture()
+            self.button.config(text="Stop Capture", command=self.stop_capture, bg='#FF0000')
+        else:
+            messagebox.showerror("Error", "Please enter a label for the image.")
 
     def stop_capture(self):
-        self.is_capturing = False
-        self.capture_button.configure(text="Capture", command=self.start_capture)
-
-        if self.timer_id is not None:
-            self.window.after_cancel(self.timer_id)
-            self.timer_id = None
-
+        self.image_processing.stop_capture()
+        self.button.config(text="Start Capture", command=self.start_capture, bg='#00FF00')
 
 if __name__ == "__main__":
-    window = tk.Tk()
-    app = App(window)
-    window.mainloop()
+    app = MainApplication()
+    app.mainloop()
