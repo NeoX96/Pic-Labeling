27e12d84db9ee668893f94e7b0572573a615bfc4
Valentin <valentin@wuerfelmail.de>
2023-03-04 04:31:16 +0100

    based on resoltion changes

--- classes/videocapture.py
+++ classes/videocapture.py
@@ -41,16 +41,29 @@ class VideoCapture(tk.Frame):
         self.canvas.bind("<B1-Motion>", self.on_mouse_move)
         self.canvas.bind("<ButtonRelease-1>", self.on_mouse_up)
 
+    def start_update(self):
         self.update()
-
     
     def update(self):
         if self.cap.isOpened():
             ret, frame = self.cap.read()
             if ret:
-                if self.cropping:
-                    self.cropped_frame = frame[int(self.y1)+2:int(self.y2)-2, int(self.x1)+2:int(self.x2)-2]
-                    cv2.rectangle(frame, (self.x1, self.y1), (self.x2, self.y2), (0, 255, 0), 2)
+                original_width = self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+                original_height = self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+                width_entry_value = self.master.width_entry.get()
+                height_entry_value = self.master.height_entry.get()
+                if width_entry_value.isdigit() and height_entry_value.isdigit() and int(width_entry_value) > 20 and int(height_entry_value) > 10:
+                    width = int(width_entry_value)
+                    height = int(height_entry_value)
+                    frame = cv2.resize(frame, (width, height))
+
+                    if self.cropping:
+                        self.cropped_frame = frame[
+                            int(self.y1 * height / original_height)+2:int(self.y2 * height / original_height)-2,
+                            int(self.x1 * width / original_width)+2:int(self.x2 * width / original_width)-2
+                        ]
+                        cv2.rectangle(frame, (int(self.x1 * width / original_width), int(self.y1 * height / original_height)), 
+                              (int(self.x2 * width / original_width), int(self.y2 * height / original_height)), (0, 255, 0), 2)
                 
                 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 frame = np.array(frame)
@@ -61,6 +74,7 @@ class VideoCapture(tk.Frame):
                 self.canvas.itemconfig(self.video_feed, image=frame)
                 self.canvas.image = frame
         self.after(30, self.update)
+
 
 
     def on_mouse_down(self, event):
