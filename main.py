ddd7b8528c966696ded2011e5b83fd2908a95daf
Valentin <valentin@wuerfelmail.de>
2023-03-04 02:25:00 +0100

    adding resolution support

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
+import cv2
 from classes.videocapture import VideoCapture
 from classes.imageprocessing import ImageProcessing
 
@@ -17,6 +18,11 @@ class MainApplication(tk.Tk):
         self.label_interval = tk.Label(self, text="Enter Capture Interval (ms):", bg='#2E2E2E', fg='white')
         self.entry_interval = tk.Entry(self, bg='#333333', fg='white')
         self.button = tk.Button(self, text="Start Capture", command=self.start_capture, bg='#00FF00', fg='black')
+
+        self.resolution_label = tk.Label(self, text="Resolution:", bg='#2E2E2E', fg='white')
+        self.width_entry = tk.Entry(self, bg='#333333', fg='white')
+        self.x_label = tk.Label(self, text="x", bg='#2E2E2E', fg='white')
+        self.height_entry = tk.Entry(self, bg='#333333', fg='white')
         
         self.label_image.pack( pady=(30, 1) )
         self.entry_image.pack(pady=(0, 20))
@@ -24,11 +30,22 @@ class MainApplication(tk.Tk):
         self.entry_interval.pack(pady=0)
         self.button.pack(pady=30)
 
+        self.resolution_label.pack(pady=0)
+        self.width_entry.pack( pady=0)
+        self.x_label.pack(pady=0)
+        self.height_entry.pack(pady=0)
+
         self.video_capture.pack(pady=30, padx=30)
+
+        self.width_entry.insert(0, str(int(self.video_capture.cap.get(cv2.CAP_PROP_FRAME_WIDTH))))
+        self.height_entry.insert(0, str(int(self.video_capture.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
+
 
 
     def start_capture(self):
         label = self.entry_image.get()
+
+
         if label:
             interval = self.entry_interval.get()
             if interval:
@@ -46,3 +63,4 @@ class MainApplication(tk.Tk):
 if __name__ == "__main__":
     app = MainApplication()
     app.mainloop()
+
