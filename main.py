01804456f2f229cc0ee32c0d07019dfa1195ca44
Valentin <valentin@wuerfelmail.de>
2023-05-09 16:32:39 +0200

    added filebrowser to loadmodel

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import tkinter as tk
+import os
 import customtkinter as ctk
 from customtkinter import set_default_color_theme
 from tkinter import messagebox
 import cv2
 from classes.videocapture import VideoCapture
 from classes.imageprocessing import ImageProcessing
+from classes.loadmodel import LoadModel
 
 set_default_color_theme("dark-blue")
 
@@ -24,7 +26,7 @@ class MainApplication(tk.Tk):
         title_label = tk.Label(self, text="KI-MCR-Projekt", font=("Arial", 60))
         title_label.pack(pady=(100, 0))
 
-        self.load_model_button = ctk.CTkButton(self, text="Load Model", command=self.load_model, font=("Arial", 40))
+        self.load_model_button = ctk.CTkButton(self, text="Load Model", command=self.init_loader_gui, font=("Arial", 40))
         self.capture_images_button = ctk.CTkButton(self, text="Capture Images", command=self.init_capture_gui, font=("Arial", 40))
 
         self.load_model_button.configure(
@@ -56,17 +58,12 @@ class MainApplication(tk.Tk):
         author_label.pack(side=tk.RIGHT, padx=20, pady=10)
 
 
-
-
-
-    def load_model(self):
-        # Platzhalter für das Laden des Modells
-        print ("Model load GUI")
-        pass
-
     def show_main_buttons(self):
         """ Show the main buttons and hide the capture buttons. """
-        self.video_capture.stop_update()
+        # if tkinter.tkapp has object video_capture, stop the video capture
+        if hasattr(self, "video_capture"):
+            self.video_capture.stop_update()
+            
         for widget in self.winfo_children():
             widget.pack_forget()
 
@@ -183,6 +180,69 @@ class MainApplication(tk.Tk):
         # Start updating the video capture widget
         self.video_capture.start_update()
 
+    def init_loader_gui(self):
+        """Initialize the GUI for load Model with filesearch and load button."""
+        self.loadmodel = LoadModel(self)
+        for widget in self.winfo_children():
+            widget.pack_forget()
+
+        # Back button
+        self.back_button = ctk.CTkButton(self, text="Back", command=self.show_main_buttons, fg_color="red")
+        self.back_button.pack(pady=(10, 30))
+
+        # Find all .h5 and .txt files in directory
+        self.h5_files = [] # empty list for .h5 files
+        self.txt_files = [] # empty list for .txt files
+
+        # search for .h5 and .txt files in the current directory
+        for file in os.listdir():
+            if file.endswith(".h5"):
+                self.h5_files.append(file)
+            elif file.endswith(".txt"):
+                self.txt_files.append(file)
+
+        self.loading_frame = ctk.CTkFrame(self)
+
+        # create a label and a dropdown menu for selecting the .h5 file
+        self.h5_frame = ctk.CTkFrame(self)
+        self.h5_label = ctk.CTkLabel(self.h5_frame, text="Select .h5 file:")
+        if len(self.h5_files) == 0:
+            self.h5_variable = ctk.StringVar(value="  No .h5 files found.  ")
+            self.load_model_button = ctk.CTkButton(self.loading_frame, text="Load Model", command=self.loadmodel.load_model, fg_color="#FF9000", state="disabled")
+        else:
+            self.h5_variable = ctk.StringVar(value=self.h5_files[0]) # set the default value to the first .h5 file in the list
+            self.load_model_button.configure(state="normal")
+        self.h5_dropdown = ctk.CTkOptionMenu(self.h5_frame, variable=self.h5_variable, values=self.h5_files)
+        self.browse_h5_button = ctk.CTkButton(self.h5_frame, text="Browse", command=self.loadmodel.browse_file_h5)
+
+        # create a label and a dropdown menu for selecting the .txt file
+        self.txt_frame = ctk.CTkFrame(self)
+        self.txt_label = ctk.CTkLabel(self.txt_frame, text="Select .txt file:")
+        if len(self.txt_files) == 0:
+            self.txt_variable = ctk.StringVar(value="No .txt files found.")
+            self.load_model_button.configure(state="disabled")
+        else:
+            self.txt_variable = ctk.StringVar(value=self.txt_files[0]) # set the default value to the first .txt file in the list
+            self.load_model_button.configure(state="normal")
+        self.txt_dropdown = ctk.CTkOptionMenu(self.txt_frame, variable=self.txt_variable, values=self.txt_files)
+        self.browse_txt_button = ctk.CTkButton(self.txt_frame, text="Browse", command=self.loadmodel.browse_file_txt)
+
+        # pack the widgets into the frames
+        self.h5_label.pack(pady=10)
+        self.h5_dropdown.pack(pady=10)
+        self.browse_h5_button.pack(pady=10, padx=(10,0))
+
+        self.txt_label.pack(pady=10)
+        self.txt_dropdown.pack(pady=10)
+        self.browse_txt_button.pack(pady=10, padx=(10,0))
+
+        self.h5_frame.pack(padx=20, pady=10, fill="x")
+        self.txt_frame.pack(padx=20, pady=10, fill="x")
+        self.loading_frame.pack(padx=20, pady=10, fill="x")
+
+        self.load_model_button.pack(pady=10)
+
+
 
     def reset_crop(self):
         self.video_capture.reset_crop()
@@ -191,7 +251,6 @@ class MainApplication(tk.Tk):
         if not self.stop_counter:
             self.button.configure(text="Stop Capture: " + str(self.image_processing.counter))
             self.after(200, self.update_counter)
-
 
     def validate_resolution_callback(self, *args):
         """Validate the entered width value and calculate the height based on the aspect ratio of the camera."""
@@ -226,8 +285,6 @@ class MainApplication(tk.Tk):
             self.width_entry.delete(0, tk.END)
             self.width_entry.insert(0, str(int(self.video_capture.cap.get(cv2.CAP_PROP_FRAME_WIDTH))))
 
-
-
     def start_capture(self):
         """Start capturing images and save them to the images folder."""
         label = self.entry_image.get()
@@ -253,7 +310,11 @@ class MainApplication(tk.Tk):
         self.button.configure(text="Start Capture", command=self.start_capture, fg_color="green")
 
 
+
+
 if __name__ == "__main__":
     app = MainApplication()
     app.mainloop()
+
+
 
